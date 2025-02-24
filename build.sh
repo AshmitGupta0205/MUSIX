98a1df6a28b6045ccff8ab5ba2ce865027930298
Ashmit Gupta <ashmit0205@gmail.com>
2025-02-25 01:53:52 +0530

    Fixed PyAudio dependencies for Render

--- build.sh
+++ build.sh
@@ -1,10 +1,15 @@
-#!/bin/bash
 
 # Update package list and install system dependencies
-apt-get update && apt-get install -y portaudio19-dev libasound2-dev ffmpeg
+apt-get update && apt-get install -y \
+    portaudio19-dev \
+    libasound2-dev \
+    libffi-dev \
+    ffmpeg \
+    python3-dev \
+    gcc
 
 # Ensure pip and virtual environment are up to date
-pip install --upgrade pip
+pip install --upgrade pip setuptools wheel
 
 # Install Python dependencies from requirements.txt
 pip install -r requirements.txt
