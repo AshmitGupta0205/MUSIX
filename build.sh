58b5347191127e26edd5217b0efb9762ad7b6cb4
Ashmit Gupta <ashmit0205@gmail.com>
2025-02-25 02:25:51 +0530

    Updated build.sh to check requirements.txt

--- build.sh
+++ build.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
-# Update package list and install dependencies
-apt-get update && apt-get install -y portaudio19-dev ffmpeg
+# Update package list and install required dependencies
+apt-get update && apt-get install -y \
+    portaudio19-dev \
+    libasound2-dev \
+    libffi-dev \
+    ffmpeg \
+    python3-dev \
+    gcc \
+    g++ \
+    make \
+    pkg-config
 
-# Ensure pip is up to date
-pip install --upgrade pip
+# Manually install PortAudio (Fix missing headers)
+cd /tmp
+wget http://files.portaudio.com/archives/pa_stable_v190700_20210406.tgz
+tar -xvzf pa_stable_v190700_20210406.tgz
+cd portaudio
+./configure && make && make install
+cd ..
+
+# Ensure pip and virtual environment are up to date
+pip install --upgrade pip setuptools wheel
 
 # Check if requirements.txt exists before installing
 if [ -f "requirements.txt" ]; then
