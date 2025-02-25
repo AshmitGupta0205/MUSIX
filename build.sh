2e4d14969fcb2f609582d8acef77649f084e3616
Ashmit Gupta <ashmit0205@gmail.com>
2025-02-26 01:14:00 +0530

    Updated project with sounddevice changes

--- build.sh
+++ build.sh
@@ -1,13 +1,16 @@
-#!/bin/bash
-
-# Update and install necessary dependencies
+# Install system dependencies
 sudo apt-get update && sudo apt-get install -y \
-    libasound2-dev libffi-dev ffmpeg python3-dev gcc g++ make pkg-config autoconf automake wget
+    portaudio19-dev libasound2-dev libffi-dev ffmpeg python3-dev gcc g++ make pkg-config autoconf automake wget
+
+# Set environment variables for PortAudio
+export CFLAGS="-I/usr/local/include"
+export LDFLAGS="-L/usr/local/lib"
+export LD_LIBRARY_PATH="/usr/local/lib"
 
 # Ensure pip is up to date
 pip install --upgrade pip setuptools wheel
 
-# Install dependencies from requirements.txt
+# Install Python dependencies
 if [ -f "requirements.txt" ]; then
     pip install -r requirements.txt
 else
